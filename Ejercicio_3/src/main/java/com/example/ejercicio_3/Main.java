db476585f0f843146c78105862ebbf77d37dafd3
Santiago Cordoba <santicordoba9518@gmail.com>
2023-03-28 18:07:46 +0100

    1º commit de introduccion en java

---
+++ Ejercicio_3/src/main/java/com/example/ejercicio_3/Main.java
@@ -0,0 +1,24 @@
+package com.example.ejercicio_3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int resultado = suma(10, 30, 40 );
+        System.out.println(resultado);
+        coche miCoche = new coche ();
+        miCoche.AñadirPuerta();
+        System.out.println(miCoche.puertas);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+class coche {
+    public static int puertas = 5;
+
+    public void AñadirPuerta(){
+        this.puertas++;
+    }
+
+}
